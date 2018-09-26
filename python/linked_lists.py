b024b59b7daccd46fa2d21069c2be3d22848bb4f
Christoph Russ <christoph@codefour.ch>
2018-09-26 22:39:34 +0200

    adding remove methods and tests

--- python/linked_lists.py
+++ python/linked_lists.py
@@ -12,6 +12,7 @@ class SinglyLinkedList:
         new_start.next = self.start
         self.start = new_start
 
+    # this isn't really something you will want to do very often ...
     def append(self, data):
         new_end = Node(data)
         if self.empty():
@@ -21,6 +22,31 @@ class SinglyLinkedList:
         while _head.next is not None:
             _head = _head.next
         _head.next = new_end
+
+    def remove_start(self):
+        # remove a node from the start of the linked list
+        if self.empty():
+            raise Exception
+        data = self.start.data
+        self.start = self.start.next
+        return data
+
+    def remove_end(self):
+        # remove a node from the end of the linked list
+        if self.empty():
+            raise Exception
+        _head = self.start
+        if _head.next is None:
+            # only a single element in list - remove it
+            data = _head.data
+            self.start = None
+            return data
+        # more than one element in list - look 2 nexts ahead
+        while _head.next.next is not None:
+            _head = _head.next
+        data = _head.next.data
+        _head.next = None
+        return data
 
     def empty(self):
         if self.start is None:
--- python/test_linked_lists.py
+++ python/test_linked_lists.py
@@ -14,6 +14,32 @@ def test_singly_linked_list_append(N):
     print(f"Appending {int(N)} values")
     [l.append(i) for i in range(int(N))]
 
+def test_singly_linked_list_remove_start(N):
+    print("Testing SinglyLinkedList")
+    l = SinglyLinkedList()
+
+    print(f"Prepending {int(N)} values")
+    [l.prepend(i) for i in range(int(N))]
+    print(f"Removing {int(N)} values from the start")
+    [l.remove_start() for i in range(int(N))]
+    try:
+        l.remove_start()
+    except Exception:
+        print("Got expected Exception")
+
+def test_singly_linked_list_remove_end(N):
+    print("Testing SinglyLinkedList")
+    l = SinglyLinkedList()
+
+    print(f"Prepending {int(N)} values")
+    [l.prepend(i) for i in range(int(N))]
+    print(f"Removing {int(N)} values from the end")
+    [l.remove_end() for i in range(int(N))]
+    try:
+        l.remove_end()
+    except Exception:
+        print("Got expected Exception")
+
 def print_linked_list(l):
     print("Print list:")
     _head = l.start
@@ -24,3 +50,5 @@ def print_linked_list(l):
 if __name__ == "__main__":
     test_singly_linked_list_prepend(N=1000000)
     test_singly_linked_list_append(N=10000)
+    test_singly_linked_list_remove_start(N=1000000)
+    test_singly_linked_list_remove_end(N=10000)
